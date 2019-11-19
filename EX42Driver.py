3cbddc9e23e5de94c589a3c7a117c508d08a08b7
dammara <h3246531>
2019-11-18 21:04:48 -0500

    11/18/19 9:04PM - Wrote the code :)))

---
+++ EX42Driver.py
@@ -0,0 +1,28 @@
+from Ex42 import Animal, Dog, Cat, Person, Employee, Fish, Salmon, Halibut
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## ??
+satan = Cat("Satan")
+
+## ??
+mary = Person("Mary")
+
+## ??
+mary.pet = satan
+
+## ??
+frank = Employee("Frank", 120000)
+
+## ??
+frank.pet = rover
+
+## ??
+flipper = Fish()
+
+## ??
+crouse = Salmon()
+
+## ??
+harry = Halibut()
